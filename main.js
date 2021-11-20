2be974b4e10e876cdfcd60e26eceb714caa558d1
teja896 <85782635+teja896@users.noreply.github.com>
2021-11-20 17:18:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function add() {
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+//Write a code to grab the key-pressed event
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		//write a code to check the type of key pressed
+	else{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+}
+
+function aplhabetkey()
+{
+	//upload respective image with the message. 
+
+}
+function numberkey()
+{
+	
+}
+function arrowkey()
+{
+}
+function specialkey()
+{
+	
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
